2f37cc351707b22589d52047a4f160c536727c55
Charith Ranasinghe <charithfyp@gmail.com>
2018-07-22 09:58:51 +0530

    Create loosely coupled code using interfaces.

--- Loose-Coupling/Loose-Coupling/Program.cs
+++ Loose-Coupling/Loose-Coupling/Program.cs
@@ -10,6 +10,45 @@ namespace Loose_Coupling
     {
         static void Main(string[] args)
         {
+
+            CatcherService obService = new CatcherService(new CatcherA());
         }
+    }
+
+    public interface ICatcher
+    {
+        void Catch();
+    }
+
+    public class CatcherA : ICatcher
+    {
+        public void Catch()
+        {
+            Console.WriteLine("I AM Catcher A");
+            Console.ReadLine();
+        }
+    }
+
+    public class CatcherB : ICatcher
+    {
+        public void Catch()
+        {
+            Console.WriteLine("I AM Catcher B");
+            Console.ReadLine();
+        }
+    }
+
+    public class CatcherService
+    {
+
+        private ICatcher catcher;
+
+        public CatcherService(ICatcher obj)
+        {
+            this.catcher = obj;
+            catcher.Catch();
+        }
+
+
     }
 }
