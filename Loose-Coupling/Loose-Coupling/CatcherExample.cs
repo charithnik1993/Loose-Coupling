fc27c9636f8783deab88c17348803147fb91a688
Charith Ranasinghe <charithfyp@gmail.com>
2018-07-22 10:58:03 +0530

    Remove catcher example specific class from the main and shift it to a CatcherExample.cs class file

---
+++ Loose-Coupling/Loose-Coupling/CatcherExample.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loose_Coupling
+{
+    class CatcherExample
+    {
+
+    }
+
+
+    public interface ICatcher
+    {
+        void Catch();
+    }
+
+    public class CatcherA : ICatcher
+    {
+        public void Catch()
+        {
+            Console.WriteLine("I AM Catcher A");
+            Console.ReadLine();
+        }
+    }
+
+    public class CatcherB : ICatcher
+    {
+        public void Catch()
+        {
+            Console.WriteLine("I AM Catcher B");
+            Console.ReadLine();
+        }
+
+        public void ClassSpecificMehtod()
+        {
+            Console.WriteLine("Class specific method");
+            Console.ReadLine();
+        }
+    }
+
+    public class CatcherService
+    {
+
+        private ICatcher catcher;
+
+        public CatcherService(ICatcher obj)
+        {
+            this.catcher = obj;
+            catcher.Catch();
+        }
+
+
+    }
+}
